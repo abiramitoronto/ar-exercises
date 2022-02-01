9968a3ac60801e37ceccfee5fde4ed022419bd62
abiramitoronto <nagaabirami.toronto@gmail.com>
2022-02-01 16:38:53 -0500

    completed exercises 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,12 @@
 require_relative '../setup'
 
 puts "Exercise 1"
-puts "----------"
+user1 = Store.create(name: "Burnaby", annual_revenue: 300000,mens_apparel: true,womens_apparel:true)
+user2 = Store.create(name: "Richmond", annual_revenue: 1260000,mens_apparel: false,womens_apparel:true)
+user3 = Store.create(name: "Gastown", annual_revenue: 190000,mens_apparel: true,womens_apparel:false)
+puts "printing count" 
+puts Store.count(:all)
+
+
 
 # Your code goes below here ...
