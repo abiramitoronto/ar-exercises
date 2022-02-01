cd7b9dc844a94f6b5efdfb9697ee3a84883195d6
abiramitoronto <nagaabirami.toronto@gmail.com>
2022-02-01 17:33:51 -0500

    Completed exercises 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,17 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2) 
+puts "IDs Are"  
+puts @store1
+puts @store2
+
+@store1.id
+@store2.id
+
+@store1.name = 'Mike'
+
+@store1.save
+
 # Your code goes here ...
